e6b381e96312baf850348a63905b681e645b0dab
LifWS <46930172+LifWS@users.noreply.github.com>
2019-09-19 22:14:25 +0700

    first commit

---
+++ src/App.js
@@ -0,0 +1,14 @@
+import React, { Component } from 'react';
+//import CharacterCard from './CharacterCard';
+import WordCard from './WordCard';
+const word = "hello";
+class App extends Component {
+  render() {
+    return (
+      <div>
+        <WordCard value="HALIFFFFFFFFFFFFFFFFFFFFF"/>
+      </div>
+    );
+  }
+}
+export default App
